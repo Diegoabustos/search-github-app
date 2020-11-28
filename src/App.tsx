153e52b27e7edcc75867c92ad6f07b118d28d68c
diegoabustos <diego.bustos@ivoy.mx>
2020-11-28 13:03:34 -0600

    feat(search): Add bulma frame in project

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Header from './components/Header';
 
 function App() {
   return (
     <div>
-      Search App to GitHub users and Repos
+      <Header />
     </div>
   );
 }
